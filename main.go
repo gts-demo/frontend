0dcf20f8de815ab3f02ee15dcd21a458280f2d0e
Virtual Address <33305554+gts-demo@users.noreply.github.com>
2020-04-23 09:53:48 +0900

    Update main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "net/http"
     "io/ioutil"
+    "log"
 )
 
 func main() {
@@ -13,14 +14,14 @@ func main() {
 }
 
 func PingServer(w http.ResponseWriter, r *http.Request) {
-fmt.Fprintf(w, "frontend: Pong")
+    fmt.Fprintf(w, "frontend: Pong")
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("http://backend:8080")
+    resp, err := http.Get("http://backend:8080")
     body, err := ioutil.ReadAll(resp.Body)
-	  if err != nil {
-		  log.Fatalln(err)
-	  }
+    if err != nil {
+        log.Fatalln(err)
+    }
     fmt.Fprintf(w, "backend: %s", body)
 }
